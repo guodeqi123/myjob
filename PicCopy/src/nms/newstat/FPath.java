926ef86a05d04e8446294fdf1556a3d838e5d28a
Guodeqi <305531834@qq.com>
2019-08-28 09:31:00 +0800

    aa

---
+++ PicCopy/src/nms/newstat/FPath.java
@@ -0,0 +1,23 @@
+package nms.newstat;
+
+public class FPath {
+	
+	
+	public final static String scanbasePath = "D:/ForBdcom/0stat1/扫描所有库位信息";
+	
+	public final static String outDir = "D:/ForBdcom/0stat1/出库扫码记录";
+	
+	public final static 	String indir1 = "D:/ForBdcom/0stat1/入库扫码记录";
+	public final static 	String indir2 = "D:/ForBdcom/0stat1/入库扫码记录/nf";
+	
+	public final static String PNComparePath = "D:/ForBdcom/0stat1/物料档案比对.xls";
+	
+	public final static String PNStatuspath = "D:/ForBdcom/0stat1/物料档案0823.xlsx";
+	
+	
+	public final static String storePath  = "D:/ForBdcom/0stat1/store/电子台账0826.xlsx";
+	
+	public final static String u8PathNew  = "D:/ForBdcom/0stat1/u8/u8.xlsx";
+	public final static String u8PathOld  = "D:/ForBdcom/0stat1/u8/u8.xlsx";
+	
+}
